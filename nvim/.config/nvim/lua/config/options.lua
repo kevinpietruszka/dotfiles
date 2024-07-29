1758a22eb02aedaf2f48088c9de1842e31173c26
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-07-29 16:47:19 -0400

    removing extra spaces

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -16,9 +16,9 @@ vim.opt.smartcase = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
-vim.opt.tabstop = 2 
-vim.opt.shiftwidth = 2 
-vim.opt.expandtab = true 
-vim.opt.autoindent = true 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
 
 vim.opt.swapfile = false
